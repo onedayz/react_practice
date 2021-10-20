5786b9d7f51e86807831bd35114068e7fe5b8c50
Grey <blind1442@gmail.com>
2021-10-20 11:08:17 +0900

    1. 리액트 기초의 17) 컴포넌트이벤트만들기 까지

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ class App extends Component{
     constructor(props) {
         super(props);
         this.state = {
+            mode:'read',
+            selected_content_id: 2,
+            welcome: {title: 'Welcome', desc:'Hello, React!!'},
             subject:{
                 title:'WEB',
                 sub:'World Wide Web'
@@ -22,11 +25,33 @@ class App extends Component{
     }
 
     render(){
+        console.log('App render');
+        let _title, _desc = null;
+        if(this.state.mode === 'welcome'){
+            _title = this.state.welcome.title;
+            _desc = this.state.welcome.desc;
+        } else if(this.state.mode === 'read'){
+            let i = 0;
+            while(i < this.state.contents.length){
+                let data = this.state.contents[i];
+                if(data.id === this.state.selected_content_id){
+                    _title = this.state.contents[i].title;
+                    _desc = this.state.contents[i].desc;
+                    break;
+                }
+                i++;
+            }
+        }
+
         return (
             <div className="App">
-                <Subject title={this.state.subject.title} sub={this.state.subject.sub}/>
-                <TOC data={this.state.contents}></TOC>
-                <Content title="HTML" desc="HTML is HyperText MarkUp Language."></Content>
+                <Subject title={this.state.subject.title} sub={this.state.subject.sub}
+                         onChangePage={function(){this.setState({mode:'welcome'})}.bind(this)}
+                />
+                <TOC data={this.state.contents} onChangePage={function(id){
+                    this.setState({mode:'read', selected_content_id:Number(id)})
+                }.bind(this)}></TOC>
+                <Content title={_title} desc={_desc}></Content>
             </div>
         )
     }
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -2,9 +2,17 @@ import React,{Component} from "react";
 
 class Subject extends Component{
     render(){
+        console.log('Subject render');
         return (
             <header>
-                <h1>{this.props.title}</h1> {/* 주석 */}
+                <h1>
+                    <a href="/" onClick={function(e){
+                        e.preventDefault();
+                        this.props.onChangePage();
+                    }.bind(this)}>
+                        {this.props.title}
+                    </a>
+                </h1>
                 {this.props.sub}
             </header>
         );
