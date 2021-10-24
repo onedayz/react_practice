c8b5d577cce81d4195739f358d2b62eea41cb4a9
Grey <blind1442@gmail.com>
2021-10-24 15:54:39 +0900

    5. React Redux(04.Redux 도입까지)

--- src/App.js
+++ src/App.js
@@ -9,10 +9,8 @@ class App extends Component{
         return (
             <div className="App">
                 <h1>Root</h1>
-                <AddNumberRoot onClick={function(size){
-                    this.setState({number: this.state.number + size});;
-                }.bind(this)}/>
-                <DisplayNumberRoot number={this.state.number}/>
+                <AddNumberRoot/>
+                <DisplayNumberRoot/>
             </div>
         )
     }
--- src/components/AddNumber.jsx
+++ src/components/AddNumber.jsx
@@ -1,4 +1,5 @@
 import React,{Component} from "react";
+import store from '../store'
 
 class AddNumber extends Component{
     state = {size: 1}
@@ -7,7 +8,8 @@ class AddNumber extends Component{
             <div>
                 <h1>Add Number</h1>
                 <input type="button" value="+" onClick={function (e){
-                    this.props.onClick(this.state.size);
+                    store.dispatch({type:'INCREMENT', size: this.state.size})
+                    // this.props.onClick(this.state.size);
                 }.bind(this)}/>
                 <input type="text" value={this.state.size} onChange={function(e){
                     this.setState({size: Number(e.target.value)})
