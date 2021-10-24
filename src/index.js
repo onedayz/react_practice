70c08f1acfb4be70cf91b1a9c386302246e4ab3e
Grey <blind1442@gmail.com>
2021-10-24 12:27:11 +0900

    3. React Router DOM(챕터3 끝)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,103 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter, Route, Switch, NavLink, useParams} from "react-router-dom";
 
+function Home(){
+    return(
+        <div>
+            <h2>Home</h2>
+            Home...
+        </div>
+    )
+}
+
+let contents = [
+    {id:1, title: 'HTML',   description: 'HTML is...'   },
+    {id:2, title: 'JS',     description: 'JS is...'     },
+    {id:3, title: 'React',  description: 'React is...'  }
+]
+
+function Topic(){
+    let params = useParams();
+    let topic_id = Number(params.topic_id);
+    let selected_topic = {
+        title: 'Sorry',
+        description: 'Not Found'
+    }
+
+    for(let i = 0; i < contents.length; i++){
+        if(contents[i].id === topic_id){
+            selected_topic = contents[i];
+            break;
+        }
+    }
+    return (
+        <div>
+            <h3>{selected_topic.title}</h3>
+            {selected_topic.description}
+        </div>
+    )
+}
+
+function Topics(){
+    let lis = [];
+    for(let i = 0; i < contents.length; i++){
+        lis.push(
+            <li key={contents[i].id}><NavLink to={"/topics/" + contents[i].id}>{contents[i].title}</NavLink></li>
+        )
+    }
+
+    return(
+        <div>
+            <h2>Topics</h2>
+            <ul>
+                {lis}
+            </ul>
+            <Route path="/topics/:topic_id">
+                <Topic/>
+            </Route>
+            {/*<switch>*/}
+            {/*    <Route path="/topics/1">HTML is ...</Route>*/}
+            {/*    <Route path="/topics/2">JS is ...</Route>*/}
+            {/*    <Route path="/topics/3">React is ...</Route>*/}
+            {/*</switch>*/}
+        </div>
+    )
+}
+
+function Contact(){
+    return(
+        <div>
+            <h2>Contact</h2>
+            Contact...
+        </div>
+    )
+}
+
+function App(){
+    return (
+        <div>
+            <h1>Hello React Router DOM</h1>
+            <ul>
+                <li><NavLink exact to="/">Home</NavLink></li>
+                <li><NavLink to="/topics">Topics</NavLink></li>
+                <li><NavLink to="/contact">Contact</NavLink></li>
+            </ul>
+            <Switch>
+                <Route exact path="/"><Home/></Route>
+                <Route path="/topics"><Topics/></Route>
+                <Route path="/contact"><Contact/></Route>
+                <Route path="/">Not Found</Route>
+            </Switch>
+        </div>
+    );
+}
 ReactDOM.render(
-    <React.StrictMode>
+    <HashRouter>
         <App/>
-    </React.StrictMode>,
+    </HashRouter>,
     document.getElementById('root')
 );
 
