fab7fc316f9049b86904237b22342eeea602ff48
Grey <blind1442@gmail.com>
2021-10-24 16:09:57 +0900

    5. React Redux(05.리액트 컴포넌트에서 리덕스에 종속된 기능을 제거까지)

--- src/components/AddNumber.jsx
+++ src/components/AddNumber.jsx
@@ -1,5 +1,4 @@
 import React,{Component} from "react";
-import store from '../store'
 
 class AddNumber extends Component{
     state = {size: 1}
@@ -8,8 +7,7 @@ class AddNumber extends Component{
             <div>
                 <h1>Add Number</h1>
                 <input type="button" value="+" onClick={function (e){
-                    store.dispatch({type:'INCREMENT', size: this.state.size})
-                    // this.props.onClick(this.state.size);
+                    this.props.onClick(this.state.size);
                 }.bind(this)}/>
                 <input type="text" value={this.state.size} onChange={function(e){
                     this.setState({size: Number(e.target.value)})
---
+++ src/containers/AddNumber.jsx
@@ -0,0 +1,13 @@
+import AddNumber from "../components/AddNumber";
+import React,{Component} from "react";
+import store from '../store';
+
+export default class extends Component{
+    render(){
+        return(
+            <AddNumber onClick={function(size){
+                store.dispatch({type:'INCREMENT', size: size});
+            }.bind(this)}/>
+        )
+    }
+}
